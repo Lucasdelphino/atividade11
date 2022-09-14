f752d66d9d405c422698b628602f06da8e5a6218
Lucasdelphino <80285529+Lucasdelphino@users.noreply.github.com>
2022-09-14 20:57:04 -0300

    atv11

---
+++ atividade2/script.js
@@ -0,0 +1,14 @@
+function calcular() {
+    let saida = document.getElementById('saida')
+    let etanol = Number(document.getElementById('etanol').value)
+    let gasolina = Number(document.getElementById('gasolina').value)
+
+    //let soma = peso / (altura * altura)
+
+    //if (soma <= 18.5) {
+       //saida.innerHTML += `<p>Seu texte deu ${soma}. Abaixo do peso`
+    //}
+    //else if (soma >= 18.6 && soma <= 24.9) {
+        //saida.innerHTML += `<p>Seu texte deu ${soma}. Peso Ideal (Parabéns)`
+    //}
+}
